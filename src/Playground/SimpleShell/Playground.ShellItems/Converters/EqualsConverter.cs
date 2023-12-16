4d3cde892be1fe51224fb993027bbd0aed42d551
RadekVym <rvymetalik@email.cz>
2023-12-16 13:22:18 +0100

    DataTrigger sample

---
+++ src/Playground/SimpleShell/Playground.ShellItems/Converters/EqualsConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Playground.ShellItems.Converters;
+
+public class EqualsConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length == 2)
+            return values[0] == values[1];
+        return false;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
