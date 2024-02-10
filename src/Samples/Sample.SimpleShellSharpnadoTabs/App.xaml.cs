7b573ae2bc0a9e9438e2e0ce404a697ff7127a26
Radek Vymětalík <rvymetalik@email.cz>
2024-02-10 16:22:11 +0100

    Added a sample of Sharpnado Tabs and SimpleShell

---
+++ src/Samples/Sample.SimpleShellSharpnadoTabs/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.SimpleShellSharpnadoTabs
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Samples/Sample.SimpleShellSharpnadoTabs/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sample.SimpleShellSharpnadoTabs
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
